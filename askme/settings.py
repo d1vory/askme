2cc4ca263501e876c3070db907c89eaf2a57b617
D1VORY <dubchakob@gmail.com>
2020-05-27 21:41:52 +0300

    basic project structure

--- askme/settings.py
+++ askme/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
---
+++ mainapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    context = {}
+    return render(request,'mainapp/home.html',context )
