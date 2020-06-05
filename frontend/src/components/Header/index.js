9f677327c9984ddf7558aea0ff2987e431fe34a3
D1VORY <dubchakob@gmail.com>
2020-06-05 21:31:33 +0300

    header was made and styled

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,19 +1,32 @@
 import React,{Component} from 'react'
 import Logo from './Logo.js'
 import Navigation from './Navigation'
+import { makeStyles } from '@material-ui/core/styles';
+import {AppBar,Toolbar, Typography, Button, IconButton,Link} from '@material-ui/core'
+import './styles.css'
+
+
 
 export default class Header extends Component {
 
 
   render(){
     return(
-      <header >
-        <nav className="navbar-expand-lg navbar-light bg-light">
-          <Logo />
-          <Navigation />
-        </nav>
+      <AppBar position ="sticky" >
+        <Toolbar>
+          <div className="logo-holder">
+            <img className="logo-img" src={require("../../common/assets/logo-dumb.png")}/>
 
-      </header>
+          </div>
+              <Typography>
+            <Button color="inherit">Home</Button>
+            <Button color="inherit">Questions </Button>
+            <Button color="inherit">Friends</Button>
+            <Button color="inherit">Settings</Button>
+              <Button color="inherit"> Log out</Button>
+          </Typography>
+        </Toolbar>
+      </AppBar>
     )
   }
 }
