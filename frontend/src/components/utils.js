13f2029eccd8c1996f47210efb0ff2ed5d8d77bf
Dubovec <dubovec95@ukr.net>
2020-06-18 14:47:50 +0300

    add transformtimestamp function

--- frontend/src/components/utils.js
+++ frontend/src/components/utils.js
@@ -7,7 +7,34 @@
  * @param {String} date in string format
  * @returns {String} time in string format
  */
-function transformTimeStamp(date){
+var transformtimestamp = function(date){
 
+    var timeNow = Date.now();
+
+    var postedDate = new Date(date.replace('T', ' '));
+
+    postedDate = Date.parse(postedDate);
+
+    var diff = timeNow - postedDate;
+
+    var res = Math.round(diff/(1000*60*60*24));
+
+    console.log(res)
+
+    if (res < 1) {
+        return "Posted today";
+    } else if (res == 1) {
+        return "Posted yesterday";
+    } else if (1 < res && res < 31) {
+        return "Posted " + res + " days ago";
+    } else if (res > 32) {
+        return "Posted " + Math.round(res/30) + " months ago";
+    }
 
 }
+
+var date = '2020-6-15T06:28:15.677Z';
+
+var res = transformtimestamp(date);
+console.log(transformtimestamp(date))
+
