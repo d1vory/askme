4641d3f263d35dfe707765fd2a4e97194b0ef8ae
D1VORY <dubchakob@gmail.com>
2020-07-11 18:05:19 +0300

    update main.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,8 @@ import axios from 'axios'
 
 
 //axios.defaults.baseURL= 'http://127.0.0.1:8000'
-axios.defaults.baseURL= 'now-ask-me.herokuapp.com'
+//axios.defaults.baseURL= 'now-ask-me.herokuapp.com'
+axios.defaults.baseURL= ''
 
 const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 //const composeEnhances =  compose
