238752a9727510f0593d734ddca1f750e8d61507
D1VORY <dubchakob@gmail.com>
2020-07-11 15:42:43 +0300

    replaced redux devtools with just compose

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,7 +12,8 @@ import axios from 'axios'
 //axios.defaults.baseURL= 'http://127.0.0.1:8000'
 axios.defaults.baseURL= 'now-ask-me.herokuapp.com'
 
-const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+//const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhances =  compose
 
 const store = createStore(reducer, composeEnhances(
   applyMiddleware(thunk)
